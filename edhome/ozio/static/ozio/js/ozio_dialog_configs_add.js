538c8191e56b1ee44a2137574443e6e2b348d600
EdwinHe <edwinwzhe@gmail.com>
2014-04-17 12:53:13 +1000

    Revert to Previous: Create Obj by view + refresh

--- edhome/ozio/static/ozio/js/ozio_dialog_configs_add.js
+++ edhome/ozio/static/ozio/js/ozio_dialog_configs_add.js
@@ -26,33 +26,3 @@ $( "#configs_dialog_add" ).dialog({
 		duration: 300
 	},
 });
-
-$('form.form-horizontal').submit(function () {
-	var id = $(this).attr('id');
-	var obj_name = id.replace('_add_form','');
-	$('#' + obj_name + '_add_msg').remove();
-	
-    $.ajax({
-        type: $(this).attr('method'),
-        url: $(this).attr('action'),
-        data: convertFormToJSON($(this)),
-        success: function (data) {
-        	$("#configs_dialog_add").dialog("close");
-        	refreshTable_caller(obj_name);
-        },
-        error: function(data) {
-            var msg = "";
-        	msg += '<div id="' + obj_name + '_add_msg" class="alert alert-dismissable alert-warning">';
-        	msg += '<button type="button" class="close" data-dismiss="alert">×</button>';
-        	
-            var d = $.parseJSON(data.responseText);
-            for (var k in d) {
-            	msg += k + ': ' + d[k];
-            	msg += '<br>'
-            }
-            msg += '</div>';
-            $('#' + obj_name + '_add_form').prepend(msg);
-        }
-    });
-    return false;
-});
